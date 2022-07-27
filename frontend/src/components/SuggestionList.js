d10f063988e2b89851d9eb24598a12bb071ee38b
myeongu <ausrndi@gmail.com>
2022-07-28 01:31:02 +0900

    useAxios 훅을 통한 API 요청

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -2,35 +2,32 @@ import React, { useEffect, useState } from "react";
 import { Card } from "antd"
 import Suggestion from "./Suggestion";
 import { useAppContext } from "store";
-import axios from "axios";
+import useAxios from 'axios-hooks'
 import "./SuggestionList.scss"
 
 
 export default function SuggestionList({ style }) {
     const { store: { jwtToken }} = useAppContext();
-    const [userList, setUserList] = useState([]);
+    
+    const headers = { Authorization: `Bearer ${jwtToken}`}
+    const [{ data: userList, loading, error }, refetch] = useAxios({
+        url: "http://localhost:8000/accounts/suggestions/",
+        headers: headers,
+    })
 
-    useEffect(() => {
-        async function fetchUserList() {
-            const apiUrl = "http://localhost:8000/accounts/suggestions/";
-            const headers = { Authorization: `Bearer ${jwtToken}`}
-            try {
-                const { data } = await axios.get(apiUrl, { headers });
-                setUserList(data);
-                console.log(data);
-            }
-            catch (error) {
-                console.error(error);
-            }
-        }
-        fetchUserList();
-    }, []);
 
+    
     return (
         <div style={style}>
+            {loading && <div>Loading...</div>}
+            {error && <div>로딩 중 에러가 발생했습니다.</div>}
+
+            <button onClick={() => refetch()}>Reload</button>
+
             <Card title="Suggestions for you" size="small">
-                {userList.map(suggestionUser => (
-                    <Suggestion key={suggestionUser.username} suggestionUser={suggestionUser} />
+                {userList &&
+                    userList.map(suggestionUser => (
+                        <Suggestion key={suggestionUser.username} suggestionUser={suggestionUser} />
                 ))}
             </Card>
         </div>
